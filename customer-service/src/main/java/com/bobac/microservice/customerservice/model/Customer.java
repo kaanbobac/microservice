5d1ee2c8d14b2e4a238fabb54e5a7ab3c029105f
Kaan Bobac <bobackaan@gmail.com>
2021-06-30 13:59:18 +0300

    added model classes for customer service

---
+++ customer-service/src/main/java/com/bobac/microservice/customerservice/model/Customer.java
@@ -0,0 +1,24 @@
+package com.bobac.microservice.customerservice.model;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String name;
+	@OneToMany(fetch = FetchType.EAGER, orphanRemoval = true)
+	private List<Order> orders;
+}
