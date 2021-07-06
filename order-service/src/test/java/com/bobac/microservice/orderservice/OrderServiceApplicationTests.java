c0d685d95c4e4caf27bea8d9bc1a7b06ba677dfa
Kaan Bobac <bobackaan@gmail.com>
2021-07-06 17:16:57 +0300

    added eureka server

--- order-service/src/test/java/com/bobac/microservice/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/bobac/microservice/orderservice/OrderServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.bobac.microservice.orderservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"eureka.client.enabled=false"})
 class OrderServiceApplicationTests {
 
 	@Test
